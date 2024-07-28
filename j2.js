17bb25fe341d09d975a937720044afe928dcc6a5
Ahrakshith <98006343+Ahrakshith@users.noreply.github.com>
2024-07-28 22:16:05 +0530

    Add files via upload

---
+++ j2.js
@@ -0,0 +1,3 @@
+let start = ["january", "july", "march", "august"];
+let a = start.splice(0, 1);
+console.log(a);
---
+++ js1.js
@@ -0,0 +1,36 @@
+// let str = "apple";
+// if (str[0] === "a" && str.length > 3) {
+//     console.log("good string");
+// }
+// else {
+//     console.log("not a good sting");
+// }
+// if (str.length > 3 && str[0] === "a") {
+//     console.log("good string");
+// }
+// else {
+//     console.log("not a good sting");
+// }
+
+// //sting methods
+
+// let animal = " dog  ";
+// let a = animal.trim();
+// console.log(a);
+// let b = animal.toUpperCase();
+// console.log(b);
+// let c = animal.toLowerCase();
+// console.log(c);
+// let d = prompt("enter your name");
+// let e = d.toUpperCase();
+// console.log(e);
+
+// let g = "Ilovecoding";
+// let f = g.slice(4);
+// console.log(f);
+// let k = g.indexOf("I");
+// console.log(k);
+
+//array
+
+let s = ["january", "july", "march", "august"];
